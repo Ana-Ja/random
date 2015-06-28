5cacca4614db7cfc5a29989813dee0fdb16fc94c
ana <ajarauta@einsl.com>
2015-06-28 14:14:37 +0200

    cambio 2

--- random.js
+++ random.js
@@ -1,6 +1,6 @@
 var numero = Math.random();
+var str = '\n' + numero + ' Mayor que 0.5 \n'
 if (numero<= 0.5) {
-  console.log('\n' + numero + ' Menor que 0.5 \n');
-} else {
-  console.log('\n' + numero + ' Mayor que 0.5 \n');
+  str='\n' + numero + ' Menor que 0.5 \n');
 }
+  console.log(str);
